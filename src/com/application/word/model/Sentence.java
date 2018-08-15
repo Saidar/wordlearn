523f09f989134254df0d7bdb1cf7990035bb40da
nameskier <ramazanov13@gmail.com>
2018-08-15 13:50:13 +0300

    #add sentence feature

---
+++ src/com/application/word/model/Sentence.java
@@ -0,0 +1,29 @@
+package com.application.word.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sentence")
+public class Sentence implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
